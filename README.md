739518929bf6ee5e5cdc197ad8f1bf9b2dc1ecb4
unknown <you@example.com>
2025-10-08 10:46:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+"# Space1" 
+"# Space1" 
+"# s2" 
+"# Space1" 
---
+++ c_c++/exp.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+  int a;
+  float b;
+
+  printf("Hello world");
+/*  cin>>a;
+  cout<<a;
+  *///return 0;
+scanf("%d",&a);
+b=sqrt(a);
+for(int j=0;j<10;j++){
+  
+printf("%d\t",j);
+}
+printf("%f",b);
+}  
